bfeb9bb75c528c0fb84ab285967a58c2143d5c4a
vangel <vangel.hristov@yahoo.com>
2017-11-20 15:11:58 +0200

    add ng-csp to index.html to disable angular eval and inline styles,fix validation patterns in all forms

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -9,25 +9,6 @@ module.exports = function authenticationModule(db) {
 			return Object.prototype.toString.call(obj) === '[object String]';
 		};
 
-		//if (key && isString(key)) {
-		//	let keyLen = constants
-		//		.AUTH_KEY_LENGTH
-		//		.toString();
-		//
-		//	let ascii = new RegExp(['^[\x00-\x7F-]{', keyLen, '}$'].join(''));
-		//	let alphanumeric =
-		//		new RegExp(['^[0-9A-Z-]{', keyLen, '}$'].join(''), 'i');
-		//
-		//	if (!(ascii.test(key) && alphanumeric.test(key))) {
-		//		return next();
-		//	}
-		//
-		//	req.user = db('users')
-		//		.find({authKey: key});
-		//}
-		//
-		//return next();
-
 		if (!(key && isString(key))) {
 			return res.status(401);
 		}
